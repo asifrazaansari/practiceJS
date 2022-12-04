bc6f33fe03546d9760ac41a2086b8f1f1806618e
asifrazaansari <asifraza0231@gmail.com>
2022-12-04 15:02:24 +0530

    fizzbuzz

---
+++ Miscellaneous/FizzBuzz.js
@@ -0,0 +1,13 @@
+
+ var fizzBuzz = function(n) {
+    const result = []
+    for(let i=1; i<=n; i++){
+        if(i % 3 === 0 && i % 5 === 0) result.push("FizzBuzz")
+        else if(i % 3 === 0) result.push("Fizz")
+        else if(i % 5 === 0) result.push("Buzz")
+        else result.push(i + "")
+    }
+    return result
+};
+
+console.log(fizzBuzz(3))
