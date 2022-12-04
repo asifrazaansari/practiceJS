b4aecf02b58f6eb0fa3b77c18e49cd4853186d3f
asifrazaansari <asifraza0231@gmail.com>
2022-12-04 08:58:04 +0530

    quickSort

---
+++ Sorting_Algo/quickSort.js
@@ -0,0 +1,35 @@
+function partition(arr, left, right) {
+
+    function swap(arr, left, right) {
+        let temp = arr[left]
+        arr[left] = arr[right]
+        arr[right] = temp
+    }
+
+    let pivotEle = arr[right]
+
+    let swapInd = left  - 1
+
+    for(let i = left; i < right; i++){
+        if(arr[i] < pivotEle){
+            swapInd++
+            swap(arr, swapInd, i)
+        }
+    }
+    swap(arr, swapInd + 1, right)
+    return swapInd + 1
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+
+    if (left < right) {
+        let pivotIndex = partition(arr, left, right)
+
+        quickSort(arr, left, pivotIndex - 1)
+        quickSort(arr, pivotIndex + 1, right)
+    }
+
+    return arr
+}
+
+console.log(quickSort([5, 2, 7, 3, 6]))
