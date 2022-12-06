5e76ac1b1d0a6790bc5c1df66758d9c3cd89d660
asifrazaansari <asifraza0231@gmail.com>
2022-12-06 17:46:31 +0530

    unique pair sum

--- Arrays/uniquePairSum.js
+++ Arrays/uniquePairSum.js
@@ -24,4 +24,38 @@ function uniquePairSum(arrays, target) {
     return result
 }
 
-console.log(uniquePairSum([4, 2, 3, 4, 7, 8, 9, 6, 2, 14, 5, 1, 3, 1], 4))
+//console.log(uniquePairSum([4, 2, 3, 4, 7, 8, 9, 6, 2, 14, 5, 1, 3, 1], 4))
+
+
+
+function pairSum(arr, target){
+    let left = 0, right = arr.length - 1
+
+    arr.sort((a,b) =>  a - b)
+
+    while(left < right){
+        let sum  = arr[left] + arr[right]
+
+        if(sum === target){
+            return 1
+        }else if(sum < target){
+            right--
+        }else {
+            left++
+        }
+    }
+
+    return 0
+}
+
+//console.log(pairSum([0, 0, 0, 0, 0, -1, -1, -1, -1, -1], -1))
+
+function freqPairSum(arr, target){
+    const obj = {}
+
+    for(const key of arr) obj[key] = arr
+
+    console.log(obj)
+}
+
+console.log(freqPairSum([0, 0, 0, 0, 0, -1, -1, -1, -1, -1], -1))
